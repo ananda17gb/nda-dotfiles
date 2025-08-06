d5f43e0920af7f1c8a5504b0cc85b153afe446a7
ananda17gb <ananda.widigdo17@gmail.com>
2025-08-06 17:05:04 +0700

    update structure

--- dot/dualscreen.sh
+++
@@ -1,4 +0,0 @@
-xrandr --output eDP-1 --mode 1920x1080 --rate 60 --auto --primary
-
-xrandr --output eDP-1-1 --mode 1920x1080 --rate 60 --auto --primary
-xrandr --output HDMI-0 --mode 1920x1080 --rate 59.94 --right-of eDP-1-1
